40027eb290db67f2db93b3af59ea9bd4bdf2c4e8
Philip A Grim II <phil@insufficient-light.com>
2021-05-25 18:03:53 -0400

    Data class for funding inputs.

---
+++ src/main/java/live/thought/rationalize/FundingLine.java
@@ -0,0 +1,27 @@
+package live.thought.rationalize;
+
+public class FundingLine
+{
+  protected double amount;
+  protected int    accounts;
+
+  public double getAmount()
+  {
+    return amount;
+  }
+
+  public void setAmount(double amount)
+  {
+    this.amount = amount;
+  }
+
+  public int getAccounts()
+  {
+    return accounts;
+  }
+
+  public void setAccounts(int accounts)
+  {
+    this.accounts = accounts;
+  }
+}
