92dad4171cb607dcbab40ad8512d71e4f85b5f30
Philip A Grim II <phil@insufficient-light.com>
2021-05-30 15:56:35 -0400

    Refactored to base decisions on FundingActions rather than entire FundingLines at a time.

--- src/main/java/live/thought/rationalize/Rationalize.java
+++ src/main/java/live/thought/rationalize/Rationalize.java
@@ -23,14 +23,15 @@ import live.thought.thought4j.ThoughtClientInterface.Unspent;
 public class Rationalize
 {
   /** RELEASE VERSION */
-  public static final String VERSION         = "v0.1";
-  public static final int    CHUNK_SIZE      = 10000;
-  public static final int    CONF_CHUNK_SIZE = 2000;
+  public static final String  VERSION         = "v0.1";
+  public static final int     CHUNK_SIZE      = 10000;
+  public static final int     CONF_CHUNK_SIZE = 2000;
 
   /** Connection for Thought daemon */
-  private ThoughtRPCClient   client;
-  private Config             config;
-  private List<Unspent>      unspentCache;
+  private ThoughtRPCClient    client;
+  private Config              config;
+  private List<Unspent>       unspentCache;
+  private List<FundingAction> actionList      = new ArrayList<FundingAction>();
 
   public Rationalize(Config config)
   {
@@ -80,15 +81,23 @@ public class Rationalize
     }
 
     double total = 0.0;
-    int numSplits = 0;
     if (moreElectricity)
     {
-      // Validate that the source addresses have enough coin.  
+      int lineIndex = 1;
+      // Create a list of funding actions to take
       for (FundingLine line : fundingLines)
       {
-        numSplits += line.getAccounts();
-        total += line.getAmount() * numSplits;        
+        int numAccounts = line.getAccounts();
+        for (int i = 1; i <= numAccounts; i++)
+        {
+          String        name = String.format("%s-%d-%d", config.getPrefix(), lineIndex, i);
+          FundingAction act  = new FundingAction(name, line.getAmount());
+          actionList.add(act);
+          total += line.getAmount();
+        }
+        lineIndex++;
       }
+      // Validate that the source addresses have enough coin.
       double              source   = 0.0;
       Map<String, Double> balances = client.listAddressBalances(0.01);
       for (String s : sourceAddresses)
@@ -97,7 +106,7 @@ public class Rationalize
         if (null != val)
           source += val.doubleValue();
       }
-      if (total + (numSplits * 0.01) > source)  // Fudge factor for transaction fees
+      if (total + (actionList.size() * 0.01) > source) // Fudge factor for transaction fees
       {
         Console.output("Funding total specified greater than source account balance.");
         Console.output("Total: " + Double.toString(total) + ", Balance: " + Double.toString(source));
@@ -105,9 +114,9 @@ public class Rationalize
       }
       else
       {
+        Console.output("Sufficient funds verified.");
         try
-        {
-          Console.output("Sufficient funds verified.");
+        {       
           outputFile.createNewFile();
           pw = new PrintWriter(outputFile);
         }
@@ -118,78 +127,151 @@ public class Rationalize
         }
       }
     }
-    
+
     if (moreElectricity)
     {
+      double totalCached = 0.0;
+      double totalNeeded = total + (actionList.size() * 0.01); // Fudge for transaction fees
       if (config.getFundingStrategy().equals(Config.NEWEST_FIRST))
       {
-        unspentCache = getUnspentNewestFirst(total + (numSplits * 0.01), sourceAddresses); // Fudge for 
+        totalCached = getUnspentNewestFirst(totalNeeded, sourceAddresses); 
       }
       else
       {
-        unspentCache = getUnspentOldestFirst(total + (numSplits * 0.01), sourceAddresses);
+        totalCached = getUnspentOldestFirst(totalNeeded, sourceAddresses);
       }
-      if (null == unspentCache || unspentCache.isEmpty())
+      if (null == unspentCache || unspentCache.isEmpty() || totalCached < totalNeeded)
       {
         Console.output("Unable to retrieve sufficient unspent inputs.");
         moreElectricity = false;
       }
-    }  
+      else
+      {
+        if (Console.getLevel() > 0)
+        {
+          for (Unspent u : unspentCache)
+          {
+            Console.debug(u, 1);
+          }
+        }
+      }
+    }
 
+    // Handle the case of large unspent, where multiple outputs make more sense.
     if (moreElectricity)
     {
-      int lineIndex = 1;
-      for (FundingLine line : fundingLines)
+      Console.output("Searching for large unspent.");
+      boolean searchFinished = false;
+      while (!searchFinished)
       {
-        Console.output("Beginning funding line " + Integer.toString(lineIndex));
-        int accounts = line.getAccounts();
-        for (int idx = 1; idx <= accounts; idx++)
+        List<FundingAction> searchResults = new ArrayList<FundingAction>();
+        Unspent             large         = biggestUnspent();
+        double              target        = large.amount();
+
+        while (target > 1.0) // Fudge for transaction fees
         {
-          String     currentName = String.format("%s-%d-%d", config.getPrefix(), lineIndex, idx);
-          // Create a new private key and import it into the wallet for safety
-          PrivateKey accountKey  = new PrivateKey(config.isTestnet());
-          client.importPrivKey(accountKey.toString(), currentName, false);
-          // Get a public key
-          List<String> accountAddrs = client.getAddressesByAccount(currentName);
-          // Send the coin in chunks to avoid overly large transaction.
-          double       remaining    = line.getAmount();
-          double       sent         = 0.0;
-          int          chunks       = ((int) line.getAmount()) / CHUNK_SIZE + 1;
-          for (int j = 0; j < chunks; j++)
+          FundingAction fa = biggestUpTo(target, searchResults);
+          if (null == fa)
           {
-            double chunk = remaining < CHUNK_SIZE ? remaining : CHUNK_SIZE;
-            try
-            {
-              sendChunk(accountAddrs.get(0), line.getAmount());
-              remaining = remaining - chunk;
-              sent += chunk;
-              Thread.sleep(200);
-            }
-            catch (InterruptedException i)
-            {
-              // Don't care.
-            }
-            catch (ThoughtRPCException e)
-            {
-              Console.output("Error sending chunk.");
-              e.printStackTrace();
-              moreElectricity = false;
-              break;
-            }
+            break;
           }
-          if (sent > 0)
+          else
           {
-            // Write to the output file.
-            pw.append(String.format("%s,%s,%s\n", accountKey.toString(), accountAddrs.get(0), Double.toString(sent)));
-            Console.output(
-                "Sent " + Double.toString(sent) + " to account " + currentName + "(" + accountAddrs.get(0) + ")");
+            searchResults.add(fa);
+            target -= fa.getAmount();
           }
+        }
+        if (searchResults.size() == 0)
+        {
+          searchFinished = true;
+        }
+        else
+        {
+          for (FundingAction act : searchResults)
+          {
+            PrivateKey accountKey = new PrivateKey(config.isTestnet());
+            client.importPrivKey(accountKey.toString(), act.getName(), false);
+            act.setPrivateKey(accountKey.toString());
+            // Get a public key
+            List<String> accountAddrs = client.getAddressesByAccount(act.getName());
+            act.setPublicKey(accountAddrs.get(0));
+          }
+          try
+          {
+            sendLarge(large, searchResults);
+          }
+          catch (ThoughtRPCException e)
+          {
+            Console.output("@|red Error sending large transaction. |@");
+            e.printStackTrace();
+            moreElectricity = false;
+            break;
+          }
+          for (FundingAction act : searchResults)
+          {
+            pw.append(act.toString());
+            Console.output("Sent " + Double.toString(act.getAmount()) + " to account " + act.getName() + "("
+                + act.getPublicKey() + ")");
+          }
+          actionList.removeAll(searchResults);
+          if (actionList.isEmpty())
+          {
+            searchFinished = true;
+          }
+        }
+      }
+      Console.output("Finished large unspent.");
+    }
+
+    // Fund the rest with smaller inputs
+    if (moreElectricity)
+    {
+      for (FundingAction act : actionList)
+      {
+        // Create a new private key and import it into the wallet for safety
+        PrivateKey accountKey = new PrivateKey(config.isTestnet());
+        client.importPrivKey(accountKey.toString(), act.getName(), false);
+        act.setPrivateKey(accountKey.toString());
+        // Get a public key
+        List<String> accountAddrs = client.getAddressesByAccount(act.getName());
+        act.setPublicKey(accountAddrs.get(0));
+        // Send the coin in chunks to avoid overly large transaction.
+        double remaining = act.getAmount();
+        double sent      = 0.0;
+        int    chunks    = ((int) act.getAmount()) / CHUNK_SIZE + 1;
+        for (int j = 0; j < chunks; j++)
+        {
+          double chunk = remaining < CHUNK_SIZE ? remaining : CHUNK_SIZE;
+          try
+          {
+            sendChunk(act.getPublicKey(), act.getAmount());
+            remaining = remaining - chunk;
+            sent += chunk;
+            Thread.sleep(200); // Don't send too fast - give the daemon a break in between.
+          }
+          catch (InterruptedException i)
+          {
+            // Don't care.
+          }
+          catch (ThoughtRPCException e)
+          {
+            Console.output("@|red Error sending chunk. |@");
+            e.printStackTrace();
+            moreElectricity = false;
+            break;
+          }
+        }
+        if (sent > 0)
+        {
+          // Write to the output file.
+          pw.append(act.toString());
+          Console.output(
+              "Sent " + Double.toString(sent) + " to account " + act.getName() + "(" + act.getPublicKey() + ")");
         }
         if (!moreElectricity)
         {
           break;
         }
-        lineIndex += 1;
       }
     }
     if (null != pw)
@@ -229,9 +311,10 @@ public class Rationalize
     return lines;
   }
 
-  private List<Unspent> getUnspentOldestFirst(double balanceNeeded, String[] addresses)
+  private double getUnspentOldestFirst(double balanceNeeded, String[] addresses)
   {
-    List<Unspent> retval = new ArrayList<Unspent>();
+    Console.output("Fetching unspent inputs oldest first.");
+    unspentCache = new ArrayList<Unspent>();
     Console.debug("Getting block count.", 1);
     int maxConfirmations = client.getBlockCount();
     Console.debug("Found " + maxConfirmations + " blocks.", 1);
@@ -251,7 +334,7 @@ public class Rationalize
         if (u.solvable() && u.spendable())
         {
           balance += u.amount();
-          retval.add(u);
+          unspentCache.add(u);
         }
       }
       maxConfirmations = minconf - 1;
@@ -261,14 +344,15 @@ public class Rationalize
       if (minconf < 6)
         minconf = 6;
     }
-    Console.debug("Final transaction count: " + retval.size(), 1);
+    Console.debug("Final transaction count: " + unspentCache.size(), 1);
     Console.debug("Spendable balance: " + balance, 1);
-    return retval;
+    return balance;
   }
 
-  private List<Unspent> getUnspentNewestFirst(double balanceNeeded, String[] addresses)
+  private double getUnspentNewestFirst(double balanceNeeded, String[] addresses)
   {
-    List<Unspent> retval = new ArrayList<Unspent>();
+    Console.output("Fetching unspent inputs newest first.");
+    unspentCache = new ArrayList<Unspent>();
     Console.debug("Getting block count.", 1);
     int maxblocks = client.getBlockCount();
     Console.debug("Found " + maxblocks + " blocks.", 1);
@@ -289,7 +373,7 @@ public class Rationalize
         if (u.solvable() && u.spendable())
         {
           balance += u.amount();
-          retval.add(u);
+          unspentCache.add(u);
         }
       }
       minconf = maxconf + 1;
@@ -299,20 +383,20 @@ public class Rationalize
       else if (maxconf + CONF_CHUNK_SIZE > maxblocks)
         maxconf = maxblocks;
     }
-    Console.debug("Final transaction count: " + retval.size(), 1);
+    Console.debug("Final transaction count: " + unspentCache.size(), 1);
     Console.debug("Spendable balance: " + balance, 1);
-    return retval;
+    return balance;
   }
 
   private double sendChunk(String destinationAddr, double amount)
   {
     double actual = amount;
-    
+
     Console.debug("Building raw transaction.", 1);
-    List<TxInput> inputs = new ArrayList<TxInput>();
-    
-    List<Unspent> spentCache = new ArrayList<Unspent>();
-    double inputBalance = 0.0;
+    List<TxInput> inputs       = new ArrayList<TxInput>();
+
+    List<Unspent> spentCache   = new ArrayList<Unspent>();
+    double        inputBalance = 0.0;
     for (Unspent u : unspentCache)
     {
       TxInput input = new BasicTxInput(u.txid(), u.vout());
@@ -325,35 +409,109 @@ public class Rationalize
         break;
       }
     }
-     
+    if (inputBalance < amount)
+    {
+      Console.output("Not enough unspent left in cache.");
+    }
+    else
+    {
+      List<TxOutput> outputs = new ArrayList<TxOutput>();
+      TxOutput       output  = new BasicTxOutput(destinationAddr, amount);
+      outputs.add(output);
+
+      Console.debug("Creating raw transaction.", 1);
+      String transactionHex = client.createRawTransaction(inputs, outputs);
+
+      actual = send(transactionHex);
+
+      unspentCache.removeAll(spentCache);
+    }
+    return actual;
+  }
+
+  private double sendLarge(Unspent large, List<FundingAction> destination)
+  {
+    double actual = 0.0;
+
+    Console.debug("Building raw transaction.", 1);
+    List<TxInput> inputs = new ArrayList<TxInput>();
+    inputs.add(large);
+
     List<TxOutput> outputs = new ArrayList<TxOutput>();
-    TxOutput output = new BasicTxOutput(destinationAddr, amount);
-    outputs.add(output);
-    
+
+    for (FundingAction act : destination)
+    {
+      TxOutput output = new BasicTxOutput(act.getPublicKey(), act.getAmount());
+      outputs.add(output);
+    }
+
     Console.debug("Creating raw transaction.", 1);
     String transactionHex = client.createRawTransaction(inputs, outputs);
-   
+
+    actual = send(transactionHex);
+
+    unspentCache.remove(large);
+
+    return actual;
+  }
+
+  private double send(String rawTransaction)
+  {
+    double actual = 0.0;
     Console.debug("Funding raw transaction.", 1);
 
     FundRawTransactionOptions opts = new FundRawTransactionOptions();
     opts.setChangeAddress(config.getSourceAddresses()[0]);
-    
-    FundedRawTransaction funded = client.fundRawTransaction(transactionHex, opts);
+
+    FundedRawTransaction funded = client.fundRawTransaction(rawTransaction, opts);
     actual += funded.fee();
-    
+
     Console.debug("Signing raw transaction.", 1);
     String signedHex = client.signRawTransaction(funded.hex());
 
     Console.debug("Sending raw transaction.", 1);
     String txid = client.sendRawTransaction(signedHex);
-    
+
     Console.debug("Sent transaction " + txid, 1);
-    
-    unspentCache.removeAll(spentCache);
-    
+
     return actual;
   }
-  
+
+  private Unspent biggestUnspent()
+  {
+    Unspent biggest = unspentCache.get(0);
+    for (Unspent u : unspentCache)
+    {
+      if (u.amount() > biggest.amount())
+      {
+        biggest = u;
+      }
+    }
+    Console.debug("Biggest unspent: " + Double.toString(biggest.amount()), 1);
+    return biggest;
+  }
+
+  private FundingAction biggestUpTo(double max, List<FundingAction> exclude)
+  {
+    FundingAction retval = null;
+    for (FundingAction act : actionList)
+    {
+      if (!exclude.contains(act))
+      {
+        if (act.getAmount() < max && retval == null)
+        {
+          retval = act;
+        }
+        else if (act.getAmount() < max && act.getAmount() > retval.getAmount())
+        {
+          retval = act;
+        }
+      }
+    }
+
+    return retval;
+  }
+
   public static void main(String[] args)
   {
     Config      config = new Config(args);
