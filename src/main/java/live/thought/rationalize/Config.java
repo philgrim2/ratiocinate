be9c604b9343ede31a2136751ed5ec52d9a6a4db
Philip A Grim II <phil@insufficient-light.com>
2021-05-29 20:42:53 -0400

    Made property names consistent, added funding strategy.

--- src/main/java/live/thought/rationalize/Config.java
+++ src/main/java/live/thought/rationalize/Config.java
@@ -13,29 +13,35 @@ import org.apache.commons.cli.ParseException;
 
 public class Config
 {
+  public static final String OLDEST_FIRST = "oldest";
+  public static final String NEWEST_FIRST = "newest";
+  
+  
   /** Options for the command line parser. */
-  protected static final Options           options             = new Options();
+  protected static final Options           options                   = new Options();
   /** The Commons CLI command line parser. */
-  protected static final CommandLineParser gnuParser           = new GnuParser();
+  protected static final CommandLineParser gnuParser                 = new GnuParser();
   /** Default values for connection. */
-  private static final String              DEFAULT_HOST        = "localhost";
-  private static final String              DEFAULT_PORT        = "11617";
-  private static final String              DEFAULT_USER        = "user";
-  private static final String              DEFAULT_PASS        = "password";
-  private static final String              DEFAULT_PREFIX      = "Rationalize";
-  private static final String              DEFAULT_INPUT_FILE  = "rationalize_input.csv";
-  private static final String              DEFAULT_OUTPUT_FILE = "rationalize_results.csv";
+  private static final String              DEFAULT_HOST              = "localhost";
+  private static final String              DEFAULT_PORT              = "11617";
+  private static final String              DEFAULT_USER              = "user";
+  private static final String              DEFAULT_PASS              = "password";
+  private static final String              DEFAULT_PREFIX            = "Rationalize";
+  private static final String              DEFAULT_INPUT_FILE        = "rationalize_input.csv";
+  private static final String              DEFAULT_OUTPUT_FILE       = "rationalize_results.csv";
+  private static final String              DEFAULT_FUNDING_STRATEGY  = "oldest";
 
-  private static final String              HOST_PROPERTY       = "host";
-  private static final String              PORT_PROPERTY       = "port";
-  private static final String              USER_PROPERTY       = "user";
-  private static final String              PASS_PROPERTY       = "password";
-  private static final String              PREFIX_PROPERTY     = "prefix";
-  private static final String              ACCOUNT_PROPERTY    = "account";
-  private static final String              HELP_OPTION         = "help";
-  private static final String              CONFIG_OPTION       = "config";
-  private static final String              ACCOUNT_FILE_OPTION = "accountFile";
-  private static final String              OUTPUT_FILE_OPTION  = "output";
+  private static final String              HOST_PROPERTY             = "host";
+  private static final String              PORT_PROPERTY             = "port";
+  private static final String              USER_PROPERTY             = "user";
+  private static final String              PASS_PROPERTY             = "password";
+  private static final String              PREFIX_PROPERTY           = "prefix";
+  private static final String              ADDRESS_PROPERTY          = "addresses";
+  private static final String              FUNDING_STRATEGY_PROPERTY = "fundingStrategy";
+  private static final String              FUNDING_FILE_PROPERTY     = "fundingFile";
+  private static final String              OUTPUT_FILE_PROPERTY      = "output";
+  private static final String              HELP_OPTION               = "help";
+  private static final String              CONFIG_OPTION             = "config";
 
   /** Set up command line options. */
   static
@@ -45,24 +51,31 @@ public class Config
     options.addOption("u", USER_PROPERTY, true, "Thought server RPC user");
     options.addOption("p", PASS_PROPERTY, true, "Thought server RPC password");
     options.addOption("P", PREFIX_PROPERTY, true, "Prefix for created account names. (default: Rationalize)");
-    options.addOption("a", ACCOUNT_PROPERTY, true, "Thought wallet account name to obtain funds from");
-    options.addOption("A", ACCOUNT_FILE_OPTION, true, "File containing funding lines (default: rationalize_input.csv");
-    options.addOption("o", OUTPUT_FILE_OPTION, true, "File to write account keys to (default: rationalize_results.csv");
+    options.addOption("a", ADDRESS_PROPERTY, true,
+        "One or more (comma-separated) Thought addresses to obtain funds from (required)");
+    options.addOption("F", FUNDING_FILE_PROPERTY, true,
+        "File containing funding lines (default: rationalize_input.csv");
+    options.addOption("s", FUNDING_STRATEGY_PROPERTY, true,
+        "Strategy for selecting funding inputs (oldest or newest) (default: oldest");
+    options.addOption("o", OUTPUT_FILE_PROPERTY, true,
+        "File to write account keys to (default: rationalize_results.csv");
     options.addOption("h", HELP_OPTION, true, "Displays usage information");
-    options.addOption("f", CONFIG_OPTION, true, "Configuration file to load options from.  Command line options override config file.");
+    options.addOption("f", CONFIG_OPTION, true,
+        "Configuration file to load options from.  Command line options override config file.");
   }
 
-  protected String host;
-  protected int    port;
-  protected String user;
-  protected String password;
+  protected String   host;
+  protected int      port;
+  protected String   user;
+  protected String   password;
 
-  protected String prefix;
-  protected String sourceAccount;
-  protected String accountFileName;
-  protected String outputFileName;
-  
-  protected boolean testnet = false;
+  protected String   prefix;
+  protected String[] sourceAddresses;
+  protected String   fundingFileName;
+  protected String   outputFileName;
+  protected String   fundingStrategy;
+
+  protected boolean  testnet = false;
 
   public Config(String[] args)
   {
@@ -110,32 +123,52 @@ public class Config
       {
         props.setProperty(PASS_PROPERTY, commandLine.getOptionValue(PASS_PROPERTY));
       }
-      if (commandLine.hasOption(ACCOUNT_PROPERTY))
+      if (commandLine.hasOption(ADDRESS_PROPERTY))
       {
-        props.setProperty(ACCOUNT_PROPERTY, commandLine.getOptionValue(ACCOUNT_PROPERTY));
+        props.setProperty(ADDRESS_PROPERTY, commandLine.getOptionValue(ADDRESS_PROPERTY));
       }
-      if (commandLine.hasOption(ACCOUNT_FILE_OPTION))
+      if (commandLine.hasOption(FUNDING_FILE_PROPERTY))
       {
-        props.setProperty(ACCOUNT_FILE_OPTION, commandLine.getOptionValue(ACCOUNT_FILE_OPTION));
+        props.setProperty(FUNDING_FILE_PROPERTY, commandLine.getOptionValue(FUNDING_FILE_PROPERTY));
       }
-      if (commandLine.hasOption(OUTPUT_FILE_OPTION))
+      if (commandLine.hasOption(OUTPUT_FILE_PROPERTY))
       {
-        props.setProperty(OUTPUT_FILE_OPTION, commandLine.getOptionValue(OUTPUT_FILE_OPTION));
+        props.setProperty(OUTPUT_FILE_PROPERTY, commandLine.getOptionValue(OUTPUT_FILE_PROPERTY));
       }
       if (commandLine.hasOption(PREFIX_PROPERTY))
       {
         props.setProperty(PREFIX_PROPERTY, commandLine.getOptionValue(PREFIX_PROPERTY));
       }
-      
+      if (commandLine.hasOption(FUNDING_STRATEGY_PROPERTY))
+      {
+        props.setProperty(FUNDING_STRATEGY_PROPERTY, commandLine.getOptionValue(FUNDING_STRATEGY_PROPERTY));
+      }
+
       host = props.getProperty(HOST_PROPERTY, DEFAULT_HOST);
       port = Integer.parseInt(props.getProperty(PORT_PROPERTY, DEFAULT_PORT));
       user = props.getProperty(USER_PROPERTY, DEFAULT_USER);
       password = props.getProperty(PASS_PROPERTY, DEFAULT_PASS);
-      sourceAccount = props.getProperty(ACCOUNT_PROPERTY, "");
-      accountFileName = props.getProperty(ACCOUNT_FILE_OPTION, DEFAULT_INPUT_FILE);
-      outputFileName = props.getProperty(OUTPUT_FILE_OPTION, DEFAULT_OUTPUT_FILE);
-      prefix = props.getProperty(PREFIX_PROPERTY, DEFAULT_PREFIX);
       
+      fundingFileName = props.getProperty(FUNDING_FILE_PROPERTY, DEFAULT_INPUT_FILE);
+      outputFileName = props.getProperty(OUTPUT_FILE_PROPERTY, DEFAULT_OUTPUT_FILE);
+      prefix = props.getProperty(PREFIX_PROPERTY, DEFAULT_PREFIX);
+      fundingStrategy = props.getProperty(FUNDING_STRATEGY_PROPERTY, DEFAULT_FUNDING_STRATEGY);
+      
+      if (null == props.getProperty(ADDRESS_PROPERTY))
+      {
+        throw new IllegalArgumentException("Funding source address(es) not specified.");
+      }
+      else
+      {
+        String addresses = props.getProperty(ADDRESS_PROPERTY);
+        String[] split = addresses.split(",");
+        this.sourceAddresses = new String[split.length];
+        for (int i = 0; i < split.length; i++)
+        {
+          sourceAddresses[i] = split[i].trim();
+        }
+      }
+
       if (props.getProperty(PORT_PROPERTY, DEFAULT_PORT).startsWith("11"))
       {
         testnet = true;
@@ -153,14 +186,14 @@ public class Config
     }
   }
 
-  public String getSourceAccount()
+  public String[] getSourceAddresses()
   {
-    return sourceAccount;
+    return sourceAddresses;
   }
 
-  public void setSourceAccount(String sourceAccount)
+  public void setSourceAddresses(String[] sourceAddresses)
   {
-    this.sourceAccount = sourceAccount;
+    this.sourceAddresses = sourceAddresses;
   }
 
   public String getHost()
@@ -202,15 +235,15 @@ public class Config
   {
     this.password = password;
   }
-    
-  public String getAccountFileName()
+
+  public String getFundingFileName()
   {
-    return accountFileName;
+    return fundingFileName;
   }
 
-  public void setAccountFileName(String accountFileName)
+  public void setFundingFileName(String fundingFileName)
   {
-    this.accountFileName = accountFileName;
+    this.fundingFileName = fundingFileName;
   }
 
   public String getOutputFileName()
@@ -222,7 +255,7 @@ public class Config
   {
     this.outputFileName = outputFileName;
   }
-  
+
   public boolean isTestnet()
   {
     return testnet;
@@ -236,6 +269,16 @@ public class Config
   public void setPrefix(String prefix)
   {
     this.prefix = prefix;
+  }
+
+  public String getFundingStrategy()
+  {
+    return fundingStrategy;
+  }
+
+  public void setFundingStrategy(String fundingStrategy)
+  {
+    this.fundingStrategy = fundingStrategy;
   }
 
   public static void usage()
